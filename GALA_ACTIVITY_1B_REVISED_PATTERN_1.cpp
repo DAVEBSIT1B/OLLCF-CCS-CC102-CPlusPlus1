4fc4720d28c68df7f3aa217cfcc99b7ab4b1ca91
Dave Galaraga <ollcfbsitb1@gmail.com>
2024-12-17 14:50:26 +0800

    Add files via upload

---
+++ GALA_ACTIVITY_1B_REVISED_PATTERN_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+	cout<<"Revised Pattern 1 "<< endl;
+	int n = 7;
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < i; ++j){
+			cout << " ";
+  }
+	for(int k=0; k<n-i;++k){
+		if((i+k)%2==0){
+			cout << "#";
+		}
+		else{
+			cout<< "@";
+		}
+	}
+	cout <<endl;
+	}
+	
+	return 0;
+}
