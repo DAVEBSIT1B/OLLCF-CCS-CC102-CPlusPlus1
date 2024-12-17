60fbe7c10464029b5a8c51e0b6d1d55292f7bd92
Dave Galaraga <ollcfbsitb1@gmail.com>
2024-12-17 15:32:19 +0800

    GALA_ACTIVITY_1B_REVISED_PATTERN_2.cpp

--- GALA_ACTIVITY_1B_REVISED_PATTERN_2.cpp
+++ GALA_ACTIVITY_1B_REVISED_PATTERN_2.cpp
@@ -6,13 +6,13 @@ int main()
 	int rows = 7;
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < i; j++) {
-		cout<<" ";
+		cout<<"  ";
     }
     for (int c = 0; c < rows - i; c++) {
 		if (i % 2 == 0) {
-			cout << (c % 2 == 0 ? "@" : "#");
+			cout << (c % 2 == 0 ? " @ " : "#");
 		} else {
-			cout<<(c % 2 == 0 ? "#" : "$");
+			cout<<(c % 2 == 0 ? " # " : "$");
       }
     }
 		cout<<endl;
